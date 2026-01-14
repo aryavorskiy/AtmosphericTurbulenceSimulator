6b4cadbc79c882b534697bd8e27dfa66849ff968
aryavorskiy <aleksandr.iavorskii@weizmann.ac.il>
2026-01-14 16:55:12 +0200

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Statistics
 using HDF5, ProgressMeter
 using JET
 
-# include("test_constructors.jl")
-# include("test_atmosphere.jl")
-# include("test_imaging.jl")
+include("test_constructors.jl")
+include("test_atmosphere.jl")
+include("test_imaging.jl")
 include("test_jet.jl")
